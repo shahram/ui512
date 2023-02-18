a4b1d3217f7c0a199c22001060820483783cf36d
shahram <shahram.shahdadi@gmail.com>
2023-02-18 15:12:18 +0100

    Initial commit ui512.

---
+++ webapp/Component.js
@@ -0,0 +1,28 @@
+sap.ui.define([
+	// "sap/ui/base/ManagedObject"
+	"sap/ui/core/UIComponent"
+], function(
+	UIComponent
+) {
+	"use strict";
+
+	return UIComponent.extend("de.jlabs.demo.Component", {
+		metadata: {
+			"interfaces": [
+				"sap.ui.core.IAsyncContentCreation"
+			],
+			manifest: "json"
+/* 			"rootView": {
+				"viewName": "de.jlabs.demo.view.App",
+				"type": "XML",
+				"id": "app"
+			} */
+		},
+		init: function () {
+			UIComponent.prototype.init.apply(
+				this,
+				arguments
+			);
+		}
+	});
+});
